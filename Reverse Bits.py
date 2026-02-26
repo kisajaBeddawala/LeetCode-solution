2b4db5a66454486029b775e7ff7359ab8c31a15f
Kisaja Beddawala <kisajab72@gmail.com>
2026-02-26 16:33:00 +0530

    solved Reverse Bits problem

---
+++ Reverse Bits.py
@@ -0,0 +1,10 @@
+# Reverse Bits
+
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(format(n,'032b')[::-1],2)
+        
