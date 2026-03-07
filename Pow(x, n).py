49ebeb55fe4f5ff0ce31b7412f0e712c05d8ba73
Kisaja Beddawala <kisajab72@gmail.com>
2026-03-07 20:17:25 +0530

    solved Pow(x, n) problem

---
+++ Pow(x, n).py
@@ -0,0 +1,11 @@
+# Pow(x, n)
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return pow(x,n)
+        
